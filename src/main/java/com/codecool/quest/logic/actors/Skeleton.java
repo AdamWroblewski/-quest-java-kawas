08f0413346d5df887610f09df3667d44abb5c7d1
Adam Sz <adamsz303@gmail.com>
2020-01-14 09:55:23 +0100

    Add overriden simple move

--- src/main/java/com/codecool/quest/logic/actors/Skeleton.java
+++ src/main/java/com/codecool/quest/logic/actors/Skeleton.java
@@ -1,15 +1,35 @@
 package com.codecool.quest.logic.actors;
 
 import com.codecool.quest.logic.Cell;
+import com.codecool.quest.logic.CellType;
 import com.codecool.quest.logic.actors.Actor;
 
 public class Skeleton extends Actor {
+    private boolean direction;
     public Skeleton(Cell cell) {
         super(cell);
+        direction = true;
     }
 
     @Override
     public String getTileName() {
         return "skeleton";
+    }
+
+    @Override
+    public void move(int dx, int dy) {
+        Cell currentCell = getCell();
+        Cell nextCell = currentCell.getNeighbor(dx, dy);
+        if(direction) {
+            nextCell = currentCell.getNeighbor(1, 0);
+            direction = false;
+        } else {
+            nextCell = currentCell.getNeighbor(-1, 0);
+            direction = true;
+        }
+
+        currentCell.setActor(null);
+        nextCell.setActor(this);
+        setCEll(nextCell);
     }
 }
