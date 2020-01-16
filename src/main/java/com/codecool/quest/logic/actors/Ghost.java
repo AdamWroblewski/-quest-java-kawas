b9499b980d9acc91a1c6acee882e522b79451175
Adam Sz <adamsz303@gmail.com>
2020-01-16 12:46:53 +0100

    Refactor tab to spaces

--- src/main/java/com/codecool/quest/logic/actors/Ghost.java
+++ src/main/java/com/codecool/quest/logic/actors/Ghost.java
@@ -4,30 +4,30 @@ import com.codecool.quest.logic.Cell;
 import com.codecool.quest.logic.CellType;
 
 public class Ghost extends Enemy {
-	Ghost(Cell cell){
-		super(cell);
-		stateName = "ghost1";
-		health = 50;
-	}
+    Ghost(Cell cell){
+        super(cell);
+        stateName = "ghost1";
+        health = 50;
+    }
 
-	@Override
-	public void move(int dx, int dy) {
-		Cell nextCell = cell.getNeighbor(dx, dy);
-		if ((nextCell.getType().equals(CellType.FLOOR) && nextCell.getActor() == null) || nextCell.getType().equals(CellType.OPENEDDOOR)){
-			cell.setActor(null);
-			nextCell.setActor(this);
-			cell = nextCell;
-		}
+    @Override
+    public void move(int dx, int dy) {
+        Cell nextCell = cell.getNeighbor(dx, dy);
+        if ((nextCell.getType().equals(CellType.FLOOR) && nextCell.getActor() == null) || nextCell.getType().equals(CellType.OPENEDDOOR)){
+            cell.setActor(null);
+            nextCell.setActor(this);
+            cell = nextCell;
+        }
 
-		if(stateName.equals("ghost1") )
-			stateName = "ghost2";
-		else
-			stateName = "ghost1";
-	}
+        if(stateName.equals("ghost1") )
+            stateName = "ghost2";
+        else
+            stateName = "ghost1";
+    }
 
-	@Override
-	public boolean isPlayer() {
-		return false;
-	}
+    @Override
+    public boolean isPlayer() {
+        return false;
+    }
 
 }
