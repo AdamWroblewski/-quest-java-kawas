846e7b1613151775b3f923536a3026fa36306f01
Adam Sz <adamsz303@gmail.com>
2020-01-27 22:28:19 +0100

    Add ghost state for long distance position

--- src/main/java/com/codecool/quest/logic/actors/Ghost.java
+++ src/main/java/com/codecool/quest/logic/actors/Ghost.java
@@ -5,6 +5,8 @@ import com.codecool.quest.logic.CellType;
 import com.codecool.quest.logic.GameRandom;
 
 public class Ghost extends Enemy {
+    private boolean hidden = true;
+
     public Ghost(Cell cell){
         super(cell);
         stateName = "ghost1";
@@ -16,14 +18,23 @@ public class Ghost extends Enemy {
     public void move(int dx, int dy) {
         super.move(dx, dy);
 
+        if(hidden){
+            stateName = "ghostHidden";
+            return;
+        }
         if(stateName.equals("ghost1") )
             stateName = "ghost2";
         else
             stateName = "ghost1";
     }
     @Override
-    public void moveToPlayer(Player player, GameRandom gameRandom){
+    public double moveToPlayer(Player player, GameRandom gameRandom){
+        double distanceToPlayer = super.moveToPlayer(player, gameRandom);
+        hidden = true;
+        if(distanceToPlayer < 5.5)
+            hidden = false;
 
+        return distanceToPlayer;
     }
 
     @Override
